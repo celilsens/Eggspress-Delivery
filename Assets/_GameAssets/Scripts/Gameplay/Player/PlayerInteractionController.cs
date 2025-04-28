b76b13593afc741124f70e51ed1eb1d77532935f
celilsens <celilsens34@gmail.com>
2025-04-28 04:20:28 +0300

    v0.19 Cat AI Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
-
+    [SerializeField] private Transform _playerVisualTransform;
     private PlayerController _playerControler;
+    private Rigidbody _playerRigidbody;
     void Awake()
     {
         _playerControler = GetComponent<PlayerController>();
+        _playerRigidbody = GetComponent<Rigidbody>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -17,8 +19,16 @@ public class PlayerInteractionController : MonoBehaviour
     }
     void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.TryGetComponent<IBoostable>(out var boostable)){
+        if (other.gameObject.TryGetComponent<IBoostable>(out var boostable))
+        {
             boostable.Boost(_playerControler);
+        }
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+        {
+            damageable.GiveDamage(_playerRigidbody, _playerVisualTransform);
         }
     }
 }
--- Assets/_GameAssets/Scripts/UI/Popups/WinPopup.cs
+++ Assets/_GameAssets/Scripts/UI/Popups/WinPopup.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WinPopup : MonoBehaviour
 {
